eca164050d44e8347477b0e901870b4772f41e02
InsubKim <kis0802@neonesia.net>
2020-11-20 09:11:53 +0700

    2020.11.20

---
+++ app.js
@@ -0,0 +1,48 @@
+// Copyright 2017 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+// [START gae_node_request_example]
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
+
+module.exports = app;
+
+// var cron = require('node-schedule');
+// var rule = new cron.RecurrenceRule();
+// rule.second = 5;
+// cron.scheduleJob(rule, function(){
+//     console.log(new Date(), 'The 5th second of the minute.');
+// });
+
+// var cron2 = require('node-schedule');
+// var rule2 = new cron2.RecurrenceRule();
+// rule2.second = 3;
+// cron2.scheduleJob(rule2, function(){
+//     console.log(new Date(), 'The 3th second of the minute.');
+// });
